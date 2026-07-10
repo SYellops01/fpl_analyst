9806f1ecab72ebeef52cac9ee13d49686fb350d0
SYellops01 <sam.yellops@gmail.com>
2026-07-10 16:06:20 +0100

    Add functions to retrieve Snowflake credentials

---
+++ local/credentials.py
@@ -0,0 +1,8 @@
+def get_sf_username():
+    return '<INSERT SF USERNAME>'
+
+def get_sf_password():
+    return '<INSERT SF PASSWORD>'
+
+def get_sf_acc_identifier():
+    return '<INSERT ACCOUNT IDENTIFIER>'
