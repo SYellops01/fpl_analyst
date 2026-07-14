63c23d7d323a2e29e9c1120bc453a170838c0089
SYellops01 <sam.yellops@gmail.com>
2026-07-14 15:01:58 +0100

    Update keep_columns to include total_points

--- local/01_load_to_staging.py
+++ local/01_load_to_staging.py
@@ -111,8 +111,8 @@ def fetch_gw_stats(path, player_ids):
     '''
     Extracts all gameweek scores for players from events API for all specified player_ids
     '''
-    keep_columns = ['starts', 'minutes','goals_scored','assists','clean_sheets','goals_conceded','own_goals','penalties_saved',
-                'penalties_missed','yellow_cards','red_cards', 'saves', 'bonus','bps','influence', 
+    keep_columns = ['starts', 'minutes', 'total_points', 'goals_scored', 'assists','clean_sheets', 'goals_conceded', 'own_goals', 'penalties_saved',
+                'penalties_missed', 'yellow_cards', 'red_cards', 'saves', 'bonus','bps','influence', 
                 'creativity', 'threat', 'ict_index', 'defensive_contribution', 'expected_goals', 
                 'expected_assists', 'expected_goal_involvements', 'expected_goals_conceded']
     
